f28cebed954c45ebfb58a0c7dc6b26ec41a252a8
Tobias Ravenschlag <tobias.ravenschlag@uni-oldenburg.de>
2024-08-10 01:57:00 +0200

    add first i3 config and make vim useabel for bython

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,30 @@
 return{
+    {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.6",
         dependencies = {"nvim-lua/plenary.nvim"},
-      config = function()
+        config = function()
             -- Konfiguriere Tastenkombinationen für Telescope
-        vim.keymap.set('n', '<Leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
-        vim.keymap.set('n', '<Leader>fo', '<cmd>Telescope oldfiles<cr>', { noremap = true, silent = true })
-        vim.keymap.set('n', '<Leader>fl', '<cmd>Telescope live_grep<cr>', { noremap = true, silent = true })
+            vim.keymap.set('n', '<Leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
+            vim.keymap.set('n', '<Leader>fo', '<cmd>Telescope oldfiles<cr>', { noremap = true, silent = true })
+            vim.keymap.set('n', '<Leader>fl', '<cmd>Telescope live_grep<cr>', { noremap = true, silent = true })
+
+        end},
+    {
+        'nvim-telescope/telescope-ui-select.nvim',
+
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                        }
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
 
         end
-
     }
+
+}
