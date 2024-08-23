c1592de9f9d47065334283b96bf3171c63024775
Tobias Ravenschlag <tobias.ravenschlag@uni-oldenburg.de>
2024-08-23 14:44:03 +0200

    add wezterm config and set groovebox color

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+
+local wezterm = require 'wezterm';
+
+return {
+  color_scheme = "Gruvbox (Gogh)", 
+
+  -- Optionale weitere Einstellungen
+  font = wezterm.font("JetBrains Mono"),  -- Beispielhafte Schriftart
+  font_size = 18.0,
+}
