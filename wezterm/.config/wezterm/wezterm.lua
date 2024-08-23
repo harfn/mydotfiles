4c457741bdee6b768f20b5c10e31752a73163fbd
Tobias Ravenschlag <tobias.ravenschlag@uni-oldenburg.de>
2024-08-23 22:39:16 +0200

    add wezterm config and set groovebox color

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,10 +1,10 @@
-
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 return {
-  color_scheme = "Gruvbox (Gogh)", 
-
-  -- Optionale weitere Einstellungen
-  font = wezterm.font("JetBrains Mono"),  -- Beispielhafte Schriftart
-  font_size = 18.0,
+    color_scheme = "Gruvbox (Gogh)",
+    color_scheme = "Ayu Mirage",
+    --window_background_image = "/home/tobias/mydotfiles/wezterm/.config/wezterm/rebel-princess-ian-king.jpg",
+    -- Optionale weitere Einstellungen
+    font = wezterm.font("JetBrains Mono"), -- Beispielhafte Schriftart
+    font_size = 18.0,
 }
