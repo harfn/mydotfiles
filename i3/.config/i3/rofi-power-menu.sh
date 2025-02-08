3883de3c80dd94a550ae05b7edda956dac9f27ba
tobias <tobias.ravenschal@uni-oldenburg.de>
2025-02-08 20:28:38 +0100

    Stelle alte Version von rofi-power-menu.sh und toggle_style.sh wieder her

--- i3/.config/i3/rofi-power-menu.sh
+++ i3/.config/i3/rofi-power-menu.sh
@@ -1,1 +1,22 @@
-xclip -selection clipboard -o > datei.txt
+#!/bin/bash
+
+selected_option=$(echo -e "Shutdown\nLock\nLogout\nReboot" | rofi -dmenu -i -p "Power Menu")
+
+case $selected_option in
+    Shutdown)
+        systemctl poweroff
+        ;;
+    Lock)
+        i3lock --image ~/Pictures/Backgrounds/lock.png --scale
+
+        ;;
+    Logout)
+        i3-msg exit
+        ;;
+    Reboot)
+        systemctl reboot
+        ;;
+    *)
+        ;;
+esac
+
