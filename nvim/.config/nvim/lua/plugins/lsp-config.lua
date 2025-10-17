ec79b4c2ce9af802f4c54847b262828b739f3be1
tobias <you@example.com>
2025-10-18 00:54:43 +0200

    fix a bug in nvim

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -20,33 +20,61 @@ return {
 			})
 		end,
 	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.beancount.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.marksman.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.r_language_server.setup({
-				capabilities = capabilities,
-				settings = {
-					r = {
-						rpath = {
-							linux = "/usr/bin/R", -- Setze hier den Pfad, den du mit "which R" erhalten hast
-						},
-					},
-				},
-			})
-		end,
-	},
+
+
+
+{
+  "neovim/nvim-lspconfig",
+  config = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    local servers = {
+      lua_ls = {},
+      pyright = {},
+      beancount = {},
+      marksman = {},
+      bashls = {},
+      r_language_server = {
+        settings = {
+          r = {
+            rpath = { linux = "/usr/bin/R" },
+          },
+        },
+      },
+    }
+
+    -- Prüfen, welche API verfügbar ist
+    local has_new_api = vim.lsp and vim.lsp.config and type(vim.lsp.config) == "table"
+
+    if has_new_api then
+      -- 🌟 neue, zukunftssichere API
+      for name, opts in pairs(servers) do
+        opts.capabilities = capabilities
+        if vim.lsp.config[name] then
+          vim.lsp.config[name].setup(opts)
+        else
+          vim.notify("No config for LSP server: " .. name, vim.log.levels.WARN)
+        end
+      end
+    else
+      -- 🧩 fallback: alte API (keine Fehler, nur Warnung)
+      local ok, lspconfig = pcall(require, "lspconfig")
+      if not ok then
+        vim.notify("nvim-lspconfig not found!", vim.log.levels.ERROR)
+        return
+      end
+
+      for name, opts in pairs(servers) do
+        opts.capabilities = capabilities
+        if lspconfig[name] and lspconfig[name].setup then
+          lspconfig[name].setup(opts)
+        else
+          vim.notify("LSP server not available: " .. name, vim.log.levels.WARN)
+        end
+      end
+    end
+  end,
+},
+
+
 }
